de20454700fb276243b2d34e5665e570bb151a66
ahmed-said7 <engahmedsaid125@gmail.com>
2024-06-17 19:06:32 +0300

    origin

--- build/app.js
+++ build/app.js
@@ -29,7 +29,7 @@ exports.app.use((0, cookie_parser_1.default)());
 // cookie parser
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: "*",
+    origin: [process.env.FRONTEND],
     credentials: true,
 }));
 exports.app.use((0, express_session_1.default)({
